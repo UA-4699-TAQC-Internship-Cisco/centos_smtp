335d054502c173e307d50a227acf4218fec06c96
Liubomyr Halamaha <galamagal86@gmail.com>
2025-06-19 12:02:27 +0300

    update struct

---
+++ config/__init__.py
@@ -0,0 +1,8 @@
+"""Contains configuration files for different environments (e.g., Dev, QA, Staging, Production).
+You can use Python files (.py), JSON (.json), or YAML (.yaml)
+to store environment-specific variables like URLs,
+ credentials (handle securely, avoid hardcoding!), and database connections.
+Example:
+    environments.py could have a dictionary where keys
+    are environment names and values are dictionaries of environment-specific settings.
+"""
