765e0b66c443956d27ebf20d3129c630775106ad
Yuliia Shaposhnikova <domik339@gmail.com>
2025-06-19 20:09:27 +0300

    Updated logger

--- resources/scripts/yuliashap_mail_send_and_read.py
+++ resources/scripts/yuliashap_mail_send_and_read.py
@@ -11,19 +11,19 @@ dotenv.load_dotenv()
 logger = setup_logger()
 
 
-# def send_email(sender, recipient, subject, body, server_ip, server_port):
-#     msg = MIMEText(body)
-#     msg['Subject'] = subject
-#     msg['From'] = sender
-#     msg['To'] = recipient
-#
-#     try:
-#         server = smtplib.SMTP(server_ip, server_port)
-#         server.sendmail(sender, [recipient], msg.as_string())
-#         server.quit()
-#         logger.info("Email sent from %s to %s", sender, recipient)
-#     except Exception as e:
-#         logger.error("Failed to send email: %s", str(e))
+def send_email(sender, recipient, subject, body, server_ip, server_port):
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = recipient
+
+    try:
+        server = smtplib.SMTP(server_ip, server_port)
+        server.sendmail(sender, [recipient], msg.as_string())
+        server.quit()
+        logger.info("Email sent from %s to %s", sender, recipient)
+    except Exception as e:
+        logger.error("Failed to send email: %s", str(e))
 
 
 def read_all_emails(host, port, username, password, mail_file):
@@ -65,14 +65,14 @@ def get_last_email(host, port, username, password, mail_file):
 
 
 if __name__ == "__main__":
-    # send_email(
-    #     sender=os.getenv("LOCAL_SENDER"),
-    #     recipient=os.getenv("REMOTE_RECIPIENT"),
-    #     subject=os.getenv("EMAIL_SUBJECT"),
-    #     body=os.getenv("EMAIL_BODY"),
-    #     server_ip=os.getenv("SERVER_IP"),
-    #     server_port=int(os.getenv("SERVER_PORT", 25))
-    # )
+    send_email(
+        sender=os.getenv("LOCAL_SENDER"),
+        recipient=os.getenv("REMOTE_RECIPIENT"),
+        subject=os.getenv("EMAIL_SUBJECT"),
+        body=os.getenv("EMAIL_BODY"),
+        server_ip=os.getenv("SERVER_IP"),
+        server_port=int(os.getenv("SERVER_PORT", 25))
+    )
 
     get_last_email(
         host=os.getenv("SSH_HOST"),
